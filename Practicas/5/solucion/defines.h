453055cf70689e7443f45979db41b21ce943fd28
Santiago Pensotti <spensotti@dc.uba.ar>
2023-05-21 13:10:25 -0300

    GDT

--- Practicas/5/solucion/defines.h
+++ Practicas/5/solucion/defines.h
@@ -40,10 +40,10 @@
  * Definirlos a partir de los índices de la GDT, definidos más arriba 
  * Hint: usar operadores "<<" y "|" (shift y or) */
 
-//#define GDT_CODE_0_SEL ??
-//#define GDT_DATA_0_SEL ??
-//#define GDT_CODE_3_SEL ??
-//#define GDT_DATA_3_SEL ??
+#define GDT_CODE_0_SEL (GDT_IDX_CODE_0 << )
+#define GDT_DATA_0_SEL (GDT_IDX_DATA_0 << 3)
+#define GDT_CODE_3_SEL (GDT_IDX_CODE_3 << 3)
+#define GDT_DATA_3_SEL (GDT_IDX_DATA_3 << 3)
 
 
 // Macros para trabajar con segmentos de la GDT.
@@ -61,14 +61,14 @@
 #define GDT_BASE_HIGH(base) (uint8_t)((((uint32_t)(base)) >> 24) & 0xFF)
 
 /* COMPLETAR - Valores de atributos */ 
-//#define DESC_CODE_DATA ??
-//#define DESC_SYSTEM    ??
-//#define DESC_TYPE_EXECUTE_READ ??
-//#define DESC_TYPE_READ_WRITE   ??
+#define DESC_CODE_DATA 1
+#define DESC_SYSTEM    0
+#define DESC_TYPE_EXECUTE_READ 1010
+#define DESC_TYPE_READ_WRITE   0010
 
 /* COMPLETAR - Tamaños de segmentos */ 
-//#define FLAT_SEGM_SIZE   ??
-//#define VIDEO_SEGM_SIZE  ??
+#define FLAT_SEGM_SIZE   817 * 1024 
+#define VIDEO_SEGM_SIZE  ROWS * COLS * 2
 
 
 /* Direcciones de memoria */
--- Practicas/5/solucion/gdt.c
+++ Practicas/5/solucion/gdt.c
@@ -33,13 +33,91 @@ gdt_entry_t gdt[GDT_COUNT] = {
             .g = 0x00,
             .base_31_24 = 0x00,
         },
+        [GDT_IDX_CODE_0] =
+        {
+            // El descriptor nulo es el primero que debemos definir siempre
+            // Cada campo del struct se matchea con el formato que figura en el manual de intel
+            // Es una entrada en la GDT.
+            .limit_15_0 = GDT_LIMIT_LOW(GDT_LIMIT_4KIB(FLAT_SEGM_SIZE)),
+            .base_15_0 = GDT_BASE_LOW(0),
+            .base_23_16 = GDT_BASE_MID(0),
+            .type = DESC_TYPE_EXECUTE_READ,
+            .s = DESC_CODE_DATA,
+            .dpl = 0x00,
+            .p = 0x1,
+            .limit_19_16 = GDT_LIMIT_HIGH((GDT_LIMIT_4KIB(FLAT_SEGM_SIZE))),
+            .avl = 0x0,
+            .l = 0x0,
+            .db = 0x1,
+            .g = 0x1,
+            .base_31_24 = GDT_BASE_HIGH(0),
+        },
+        [GDT_IDX_CODE_3] =
+        {
+            .limit_15_0 = GDT_LIMIT_LOW(GDT_LIMIT_4KIB(FLAT_SEGM_SIZE)),
+            .base_15_0 = GDT_BASE_LOW(0),
+            .base_23_16 = GDT_BASE_MID(0),
+            .type = DESC_TYPE_EXECUTE_READ,
+            .s = DESC_CODE_DATA,
+            .dpl = 0x11,
+            .p = 0x1,
+            .limit_19_16 = GDT_LIMIT_HIGH((GDT_LIMIT_4KIB(FLAT_SEGM_SIZE))),
+            .avl = 0x0,
+            .l = 0x0,
+            .db = 0x1,
+            .g = 0x1,
+            .base_31_24 = GDT_BASE_HIGH(FLAT_SEGM_SIZE),
+        },
 
-    /* Completar la GDT: 
-      Es conveniente completar antes las constantes definidas en defines.h y valerse
-      de las mismas para definir los descriptores acá. Traten en lo posible de usar las 
-      macros allí definidas.
-      Tomen el descriptor nulo como ejemplo y definan el resto.
-     */
+        [GDT_IDX_DATA_0] =
+        {
+            .limit_15_0 = GDT_LIMIT_LOW(GDT_LIMIT_4KIB(FLAT_SEGM_SIZE)),
+            .base_15_0 = GDT_BASE_LOW(0),
+            .base_23_16 = GDT_BASE_MID(0),
+            .type = DESC_TYPE_READ_WRITE,
+            .s = DESC_CODE_DATA,
+            .dpl = 0x00,
+            .p = 0x1,
+            .limit_19_16 = GDT_LIMIT_HIGH((GDT_LIMIT_4KIB(FLAT_SEGM_SIZE))),
+            .avl = 0x0,
+            .l = 0x0,
+            .db = 0x1,
+            .g = 0x1,
+            .base_31_24 = GDT_BASE_HIGH(0),
+        },
+        [GDT_IDX_DATA_3] =
+        {
+            .limit_15_0 = GDT_LIMIT_LOW(GDT_LIMIT_4KIB(FLAT_SEGM_SIZE)),
+            .base_15_0 = GDT_BASE_LOW(0),
+            .base_23_16 = GDT_BASE_MID(0),
+            .type = DESC_TYPE_READ_WRITE,
+            .s = DESC_CODE_DATA,
+            .dpl = 0x11,
+            .p = 0x1,
+            .limit_19_16 = GDT_LIMIT_HIGH((GDT_LIMIT_4KIB(FLAT_SEGM_SIZE))),
+            .avl = 0x0,
+            .l = 0x0,
+            .db = 0x1,
+            .g = 0x1,
+            .base_31_24 = GDT_BASE_HIGH(0),
+        },
+      [GDT_IDX_VIDEO] = 
+      {
+        .limit_15_0 = GDT_LIMIT_LOW(VIDEO_SEGM_SIZE - 1),
+        .base_15_0 = GDT_BASE_LOW(VIDEO),
+        .base_23_16 = GDT_BASE_MID(VIDEO),
+        .type = DESC_TYPE_READ_WRITE,
+        .s = DESC_CODE_DATA,
+        .dpl = 0x00,
+        .p = 0x01,
+        .limit_19_16 = GDT_LIMIT_HIGH(VIDEO_SEGM_SIZE - 1),
+        .avl = 0x0,
+        .l = 0x0,
+        .db = 0x1,
+        .g = 0x0,
+        .base_31_24 = GDT_BASE_HIGH(VIDEO),
+      },
+
     
 };
 
