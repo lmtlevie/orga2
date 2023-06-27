c2fae3e210752d5cdcf99a905199a726185aba62
Santiago Pensotti <spensotti@dc.uba.ar>
2023-06-27 10:26:46 -0300

    Sharpen

--- Practicas/4/simd-bundle.v0.1/entregable/simd-bundle.v0.1/src/filters/Sharpen_asm.asm
+++ Practicas/4/simd-bundle.v0.1/entregable/simd-bundle.v0.1/src/filters/Sharpen_asm.asm
@@ -1,156 +1,338 @@
-section .rodata
-	sharpen: dd -1.0, -1.0, -1.0, -1.0, 9.0, -1.0, -1.0, -1.0, -1.0
-	solo_alpha: db 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255
-	pixel_negro : db 0,0,0,255
-	cut: db 255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255
-section .text
-	%define offset_pixels 16
-	%define float_size 4
-	%define pixel_size 4
-	
-
 global Sharpen_asm
+mascaraNueve: times 8 dw 9  ; [9, 9, 9, 9, 9, 9, 9, 9]
+mascaraBlackDeDosPixeles: times 2 db 0, 0, 0, 0xff
 
-
-; *src uint_8[rdi], *dst uint_8[rsi], width uint_32[rdx], height uint_32[rcx],
-; src_row_size uint_32[r8], dst_row_size uint_32[r9]
 Sharpen_asm:
 	push rbp
-	mov rbp, rsp
-	push rbx
-	push r12
-	push r13
-	push r14
-	push r15
-	push rsi
-	xor r10, r10
-
-	movdqu xmm10, [solo_alpha]
-
-	movdqu xmm13, [cut]
-    psrldq xmm13, 4
-    por xmm13, xmm10
-
-	xor r10, r10
-	top_black:
-		movups [rsi + r10], xmm10
-		add r10, 4*4                ; 4(px) * 4(bytes cada px)
-		cmp r10, r
-		jne top_black
+    mov rbp, rsp
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14 ;aca me voy a guardar el pixel que esta arriba a la izquierda
+    push r15  
+    
+        
+    xor r11, r11 ; r11 = nro de pixeles modificados de una fila. Va a ser mÃºltiplo de 2 X
+    xor r13, r13 ; r13 = nro de filas modificadas                                       Y
 
 
-	sub rdx, 2 ; 3 porque termina en ancho -3 y 1 porque no me importa el primero
-	sub rcx, 1	; sub rcx, 2  -> con esto no pierde memoria y deja la linea blanca abajo
-	sub r9, 2
-	xor r10, r10
 
-	ciclo:
-		;Si no estoy en caso r10 = altura
-		xor r11, r11 ; r11 = ii
-		pxor xmm6, xmm6 ; pixel 1
-		pxor xmm7, xmm7 ; pixel 2
-		pxor xmm8, xmm8 ; pixel 3
-		pxor xmm9, xmm9 ; pixel 4
+    mov r12, rdx
+    imul r12, 4
+    mov r9, r8  ;
+	sub r8, r12 ;src_row_padding
 
-		.cicloSharpenII:
-			cmp r11, 3
-			je finSharpen
-			xor r12, r12 ; R12 = JJ
-	
-			.cicloSharpenJJ:
-				;Tengo que cargar la mascara del sharpen , los valores [i + ii][j + jj]
-				mov r13, 12
-				imul r13, r11
-				movdqu xmm1, [sharpen + r13 + r12*4]
-				shufps xmm1, xmm1, 0x00 ; xmm1 = [a, a, a, a]
-				;Cargo sharpen [ii][jj]
-
-				;Calcular desplazamiento para i + ii
-				mov r13, r8
-				imul r13, r11
-				;Calcular desplazamiento para j + jj
-				mov r14, r12
-				imul r14, 4
-
-				add r13, r14
-
-				movq xmm0, [rdi + r13] ;|PIXEL1|PIXEL2|PIXEL3|PIXEL4|
-
-				pmovzxbd xmm2, xmm0 ; extiendo el primer pixel de byte a double word (empaquetados)
-				psrldq xmm0, 4 ; shift xmm0 4 bytes a la derecha
-				pmovzxbd xmm3, xmm0 ; extiendo el segundo pixel de byte a double word (empaquetados)
-				psrldq xmm0, 4 ; shift xmm0 4 bytes a la derecha
-								
-				cvtdq2ps xmm2, xmm2 ; convierto el primer pixel de int a float
-				cvtdq2ps xmm3, xmm3 ; convierto el segundo pixel de int a float
-				
-				mulps xmm2, xmm1
-				mulps xmm3, xmm1
-			
-		 		addps xmm6, xmm2
-				addps xmm7, xmm3
-				;; AUMENTAR JJ EN 1 Y CHEQUEAR SI ES IGUAL A 3
-
-				inc r12
-				cmp r12, 3
-				jne .cicloSharpenJJ
-				inc r11
-				jmp .cicloSharpenII
-			
-			finSharpen:
-				;Tengo que convertir los valores a int y pasarlos a 8 bits de manera saturada
-				cvtps2dq xmm6, xmm6
-				cvtps2dq xmm7, xmm7
-		
-				packssdw xmm6, xmm7
-				packuswb xmm6, xmm6
-			
-			    por xmm6, xmm10 ; alpha en 255
-				;por xmm6, xmm10 ; alpha en 255
-
-				movq [rsi + r8 + 4], xmm6
-
-				add rsi, 8 ;2 PIXELES
-				add rdi, 8 ;2 PIXELes
-
-				add r15, 8;2 PIXELES
-
-				cmp r15, r9
-				jl  ciclo
-
-				;Si cambie de fila
-				inc r10
-				sub rsi, 16
-				movdqu xmm0, [rsi + r8+ 4]
-				pand xmm0, xmm13
-				movups [rsi + r8+ 4], xmm0
-				add rsi, 16 ;2 PIXELES
-				sub rsi, 20
-				movdqu xmm0, [rsi + r8+ 4]
-				pand xmm0, xmm13
-				movups [rsi + r8+ 4], xmm0
-				add rsi, 20 ;2 PIXELES
-				xor r15, r15
-				cmp r10, rcx	
-				jne ciclo
+    
+    .ciclo:
+        cmp r13, 0
+        je .primeraFila
+        
+        mov r10, r13
+        inc r10
+        cmp r10, rcx
+        je .ultimaFila
 
 
-			fin:
-				sub rsi, 8
-				add rsi, r8
-				xor r10, r10
-				btm_black:
-					movups [rsi + r10], xmm10
+        xor r10, r10 ;ii
+        xor r15, r15 ;jj
+        
+        pxor xmm0, xmm0
 
-					add r10, 4*4                ; 4(px) * 4(bytes cada px)
-					cmp r10, r8
-					jne btm_black
-				pop rsi
-				pop r15
-				pop r14
-				pop r13
-				pop r12
-				pop rbx
-				pop rbp				
-				ret
+        ;cmp r11, 10
+        ;je .fin
 
+        jmp .aplicarFiltro
+
+
+    .primeraFila:
+        cmp r11, rdx
+        jne .noTerminaPrimeraFila
+
+        add r13, 1      ;paso a la siguiente fila
+        xor r11, r11    ;seteo el r11 en 0
+        add rdi, r8     ;salto con padding a la siguiente fila
+        add rsi, r8
+
+        jmp .ciclo
+
+
+        .noTerminaPrimeraFila:
+        mov r15, [mascaraBlackDeDosPixeles] ;puedo usar el r15 porque no me afecta lo de despues
+        mov [rsi], r15 
+
+        add r11, 2
+        add rsi, 8
+        add rdi, 8
+
+        jmp .primeraFila
+
+    .ultimaFila:
+        cmp r11, rdx
+        je .fin
+
+        mov r15, [mascaraBlackDeDosPixeles] ;puedo usar el r15 porque no me afecta lo de despues
+        mov [rsi], r15
+
+        add r11, 2
+        add rsi, 8
+        add rdi, 8
+        
+
+        jmp .ultimaFila
+
+
+    .aplicarFiltro:
+        ;primero necesito el valor desde donde voy a empezar el filtro (ii=0 jj=0)
+        mov r14, rdi
+        sub r14, r9
+        sub r14, 4     ; r14 = pixel de arriba a la izquierda
+
+        cmp r11, 0 ;veo si es el primer pixel. Si es el primero le aplico negro al primero y pixel al ultimo
+        jne .noEsPrimerPixel
+
+        push r10
+        mov r10, [mascaraBlackDeDosPixeles]
+        mov [rsi], r10
+        pop r10
+        add r14, 4
+        add rdi, 4
+        add rsi, 4
+        pxor xmm0, xmm0
+        jmp .sharpenPrimerPixel
+        
+
+        ;jmp .desdeAca
+
+        .noEsPrimerPixel:
+
+        
+        add r11, 2
+        cmp r11, rdx ;si es el ultimo menos 2 le aplico filtro al primer pixel y negro al ultimo
+        jne .noEsUltimoPixel
+
+        sub r11, 2
+        push r12
+        mov r12, [mascaraBlackDeDosPixeles]
+        mov [rsi], r12
+        pop r12
+        sub r14, 4
+        sub rdi, 4
+        sub rsi, 4
+        jmp .sharpenUltimoPixel
+
+        jmp .desdeAca
+
+
+        .noEsUltimoPixel:
+        sub r11, 2
+        jmp .sharpen
+
+    
+    .sharpen:
+        cmp r10, 3
+
+        je .siguientePixeles
+        
+        push rsi
+        mov rax, r14    ;rax = [r14]
+        mov rsi, r10    
+        imul rsi, r9    
+        add rax, rsi    ;rax = [r14 + (r10*padding)]
+
+        mov rsi, r15
+        imul rsi, 4
+        add rax, rsi    ;rax = [r14 + (r10*padding) + r15*4]
+
+        pop rsi
+
+        pmovzxbw xmm1, [rax]
+        ;jmp .siguientePixeles
+
+        cmp r10, 1
+        jne .noMul9
+        cmp r15, 1
+        jne .noMul9
+
+        PMULLW xmm1, [mascaraNueve]
+        PADDW xmm0, xmm1
+        jmp .f
+
+        .noMul9:
+        PSUBW xmm0, xmm1
+
+        .f:
+
+
+        inc r15
+        cmp r15, 3
+
+        jne .jjNoEsIguslA2
+
+        inc r10
+        xor r15, r15
+        jmp .sharpen
+
+        .jjNoEsIguslA2:
+        jmp .sharpen
+
+    .sharpenPrimerPixel:
+        cmp r10, 3
+
+        jne .seguir1
+        
+        PACKUSWB xmm0, xmm0 ;packed con saturacion, en la primera mitad me deja el resultado
+        
+        mov r10, 255
+        pinsrb xmm0, r10d, 3
+        pinsrb xmm0, r10d, 7
+
+        movq [rsi], xmm0
+        sub r14, 4
+        sub rdi, 4
+        sub rsi, 4
+        jmp .desdeAca
+        
+
+        .seguir1:
+        
+        push rsi
+        mov rax, r14    ;rax = [r14]
+        mov rsi, r10    
+        imul rsi, r9    
+        add rax, rsi    ;rax = [r14 + (r10*padding)]
+
+        mov rsi, r15
+        imul rsi, 4
+        add rax, rsi    ;rax = [r14 + (r10*padding) + r15*4]
+
+        pop rsi
+
+        pmovzxbw xmm1, [rax]
+
+        cmp r10, 1
+        jne .noMul9_1
+        cmp r15, 1
+        jne .noMul9_1
+
+        PMULLW xmm1, [mascaraNueve]
+        PADDW xmm0, xmm1
+        jmp .f2
+
+        .noMul9_1:
+        PSUBW xmm0, xmm1
+
+        .f2:
+
+
+        inc r15
+        cmp r15, 3
+
+        jne .jjNoEsIguslA2_2
+
+        inc r10
+        xor r15, r15
+        jmp .sharpenPrimerPixel
+
+        .jjNoEsIguslA2_2:
+        jmp .sharpenPrimerPixel
+
+    .sharpenUltimoPixel:
+        cmp r10, 3
+
+        jne .seguir2
+        
+        PACKUSWB xmm0, xmm0 ;packed con saturacion, en la primera mitad me deja el resultado
+        
+        mov r10, 255
+        pinsrb xmm0, r10d, 3
+        pinsrb xmm0, r10d, 7
+
+        movq [rsi], xmm0
+        add r14, 4
+        add rdi, 4
+        add rsi, 4
+        jmp .desdeAca
+        
+
+        .seguir2:
+        
+        push rsi
+        mov rax, r14    ;rax = [r14]
+        mov rsi, r10    
+        imul rsi, r9    
+        add rax, rsi    ;rax = [r14 + (r10*padding)]
+
+        mov rsi, r15
+        imul rsi, 4
+        add rax, rsi    ;rax = [r14 + (r10*padding) + r15*4]
+
+        pop rsi
+
+        pmovzxbw xmm1, [rax]
+
+        cmp r10, 1
+        jne .noMul9_2
+        cmp r15, 1
+        jne .noMul9_2
+
+        PMULLW xmm1, [mascaraNueve]
+        PADDW xmm0, xmm1
+        jmp .f3
+
+        .noMul9_2:
+        PSUBW xmm0, xmm1
+
+        .f3:
+
+
+        inc r15
+        cmp r15, 3
+
+        jne .jjNoEsIguslA2_3
+
+        inc r10
+        xor r15, r15
+        jmp .sharpenUltimoPixel
+
+        .jjNoEsIguslA2_3:
+        jmp .sharpenUltimoPixel
+
+
+    .siguientePixeles:
+        PACKUSWB xmm0, xmm0 ;packed con saturacion, en la primera mitad me deja el resultado
+        
+        mov r10, 255
+        pinsrb xmm0, r10d, 3
+        pinsrb xmm0, r10d, 7
+
+        movq [rsi], xmm0  
+        
+
+        .desdeAca:
+        add r11, 2 ;le sumo los 2 pixeles que recorri
+        add rdi, 8 ;se agregan 8 bytes
+        add rsi, 8
+
+        cmp r11, rdx
+        jne .noTerminoLaFila
+
+        add r13, 1
+        xor r11, r11
+        add rdi, r8
+        add rsi, r8
+
+
+        .noTerminoLaFila:
+        jmp .ciclo
+
+
+.fin:
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop rbp
+    ret
